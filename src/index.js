3c6a34de995a84aac192efcc1449d1956f91353f
misikira <misikirteka@gmail.com>
2023-07-19 16:16:33 +0300

    Update the object properie name

--- src/index.js
+++ src/index.js
@@ -4,36 +4,36 @@ const todoBody = document.querySelector('.todo_main_content');
 
 const todos = [
   {
-    id: 1,
-    task: 'Do Laundary',
-    status: true,
+    index: 1,
+    description: 'Do Laundary',
+    completed : true,
   },
   {
-    id: 2,
-    task: 'Go shooping',
-    status: false,
+    index: 2,
+    description: 'Go shooping',
+    completed : false,
   },
   {
-    id: 3,
-    task: 'Clean house',
-    status: true,
+    index: 3,
+    description: 'Clean house',
+    completed : true,
   },
 
   {
-    id: 3,
-    task: 'Clean house',
-    status: true,
+    index: 3,
+    description: 'Clean house',
+    completed : true,
   },
 ];
 
 const rendermytodolist = () => {
   if (todos.length > 0) {
     todos.forEach((todo) => {
-      const todoList = `<li data-id="${todo.id}" data-status="${todo.status}">
-      <label for="${todo.id}">
-      <input type="checkbox" id="${todo.id}" value="${todo.id}"
-      ${todo.status === 'complete' ? 'true' : false}/>
-      <input type="text" value="${todo.task}" readonly />
+      const todoList = `<li data-index="${todo.index}" data-completed ="${todo.completed }">
+      <label for="${todo.index}">
+      <input type="checkbox" index="${todo.index}" value="${todo.index}"
+      ${todo.completed  === 'complete' ? 'true' : false}/>
+      <input type="text" value="${todo.description}" readonly />
       </label>
           <div class="action">
           <button class="js-delete">
