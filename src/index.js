c88996c5b7446a401653c08e65476150f7eff15f
misikira <misikirteka@gmail.com>
2023-07-22 09:08:32 +0300

    fix ui issues

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 import './style.css';
+import Selector from '../modules/selectors.js';
+import Todo from '../modules/todo.js';
 
-const todoBody = document.querySelector('.todo_main_content');
+const objectSelector = new Selector();
+const objectTodo = new Todo();
 
-const todos = [];
-
-const renderTodoList = () => {
-  const todoListHtml = todos.map((todo) => `
-    <li data-index="${todo.index}" data-completed="${todo.completed}">
-      <label>
-        <input type="checkbox" ${todo.completed ? 'checked' : ''} />
-        <span>${todo.description}</span>
-      </label>
-      <button class="js-delete">
-        <i class="ri-delete-bin-fill"></i>
-      </button>
-    </li>
-  `).join('');
-
-  todoBody.querySelector('.js-todo-list').innerHTML = todoListHtml;
-};
-
-renderTodoList();
+window.addEventListener('load', () => {
+  objectTodo.dispaly();
+});
+objectSelector.todoForm.addEventListener('submit', (e) => {
+  objectTodo.addTask(e);
+});
+objectSelector.todoBody.addEventListener('click', (e) => {
+  objectTodo.handleFormAction(e);
+});
